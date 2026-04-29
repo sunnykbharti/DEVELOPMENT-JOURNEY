cede80d6d139ca7aace45acea9001f884f7eaa6f
Sunny <sunnykbharti@gmail.com>
2026-04-29 23:57:29 +0530

    Vue Navigation

--- VUE/Router/script3.js
+++ VUE/Router/script3.js
@@ -8,14 +8,46 @@ const user = {
         console.log(this.$route.params.username)
     }
 }
+const users = {
+    template : `
+        <div>
+            <router-link to="users/dashboard">Dashboard</router-link>
+            <router-link to="users/activities">Activities</router-link>
+            <h1>This is Users View</h1>
+            <router-view></router-view>
+        </div>
+    `
+}
+const dashboard = { template : "<h3> This is Dasboard View </h3>" }
+const activities = { 
+    template : `
+    <div>
+        <h2> This is Activity View </h2>
+        <button @click="goHome">Home</button>
+    </div>
+    `,
 
+    methods : {
+        goHome : function(){
+            this.$router.push("/home")
+        }
+    }
+}
 
 //mapping with Routes
 const routes = [
     {path: "/home", component: home},
     {path : "/about", component : about},
     {path : "/profile", component : profile},
-    {path : "/user/:username", component : user}
+    {path : "/user/:username", component : user},
+    {
+        path : "/users",
+        component : users,
+        children : [
+            { path : "dashboard", component : dashboard},
+            { path : "activities", component : activities}
+        ]
+    }
 ]
 
 // create router object
